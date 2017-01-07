a17d41601e39665113743fc93dba83510f16ffff
Scott Leitch <s.d.leitch@gmail.com>
2017-01-06 23:08:02 -0500

    fixed issue with scaping weird titles from XML. Sitll confused why wasnt working

--- app/models/news_article.rb
+++ app/models/news_article.rb
@@ -3,17 +3,19 @@ class NewsArticle < ActiveRecord::Base
   has_and_belongs_to_many :members
 
   def self.scrape_cbc
-    @cbc_xml = open("http://rss.cbc.ca/lineup/politics.xml")
-    stories = Hash.from_xml(@cbc_xml)['rss']['channel']['item']
+    @@cbc_xml = open("http://rss.cbc.ca/lineup/politics.xml")
+    stories = Hash.from_xml(@@cbc_xml)['rss']['channel']['item']
 
     Member.all.each do |member|
       stories.each do |story|
         if story['title'].include?(member.fullname[2..-1])  || story['description'].include?(member.fullname[2..-1])
-          article = NewsArticle.find_or_create_by(link: story["link"])
-          article.title = story["title"],
-          article.description = story['description'],
-          article.date = Date.parse(story["pubDate"]),
-          article.outlet = "CBC"
+          article = NewsArticle.find_or_create_by(
+            link: story["link"],
+            title: story["title"],
+            outlet: "CBC",
+            date: Date.parse(story["pubDate"]),
+          )
+          # article.description = story['description'],
           member.news_articles << article
           article.save!
         end
